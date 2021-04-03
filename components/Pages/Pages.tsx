cd79fe3be23903d2416a3f39685a5b2ddeeb8e06
darcyxian <darcyxianau@gmail.com>
2021-04-03 21:04:37 +1100

    little games page added

--- components/Pages/Pages.tsx
+++ components/Pages/Pages.tsx
@@ -4,6 +4,7 @@ import About from './components/About/About';
 import Resume from './components/Resume/Resume';
 import Contact from './components/Contact/Contact';
 import Blog from './components/Blog/Blog';
+import LittleGames from './components/LittleGames/LittleGames';
 import './Pages.scss';
 
 const Pages = () =>{
@@ -13,7 +14,8 @@ const Pages = () =>{
       <About active='inactive' />
       <Resume active ='inactive' />
       <Contact active ='inactive' />
-      <Blog active = 'active' />
+      <Blog active = 'inactive' />
+      <LittleGames active = 'active' />
       <div className="snowEffect"></div>
       </>
   )
---
+++ components/Pages/components/LittleGames/LittleGames.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './LittleGames.scss';
+import PageActive from '../PageActive/PageActive';
+
+
+const LittleGames = (props:{active:string}) => {
+    let className = 'defaultBg';
+    if(props.active === 'active'){
+         className += ' LittleGamesBg'
+    }
+    
+return      <PageActive className= {className} >
+               <section className="side2Container gamesContainer">
+                <h2 className="party2__body__title2">LITTLE <span className="part2__body__h2">GAMES</span>
+                    <span className="part2__body__shadowtext1">GAMES</span> 
+                   </h2>
+                   <hr className="hrLine"></hr>
+                   <h2 className="gameButton"> <a className="gameButton__a" href="./drumGame.html">Drum Kit Game</a> </h2>
+                   <h2 className="gameButton"><a className="gameButton__a" href="./simonGame.html">Simon Game </a></h2>
+            </section >
+            </PageActive>
+}
+export default LittleGames;
